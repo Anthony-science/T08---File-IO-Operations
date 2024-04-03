fb7d5478b1b356ef03841a3518f996542d8bca53
Anthony-science <163058949+Anthony-science@users.noreply.github.com>
2024-04-03 10:06:00 +0100

    Removed old file

--- dob_task.py
+++ dob_task.py
@@ -1,18 +1,16 @@
+# Please see end for an explanation for how I got this done
 # Define the input data source through relative path
 with open("Input Code Files\\Task file\\DOB.txt", 'r') as file:
     lines = file.readlines()
     cl_lines = [item.strip() for item in lines]
 
-
+# Set up bold chars
 bold_start = '\033[1m'
 bold_end = '\033[0m'
 
 print(bold_start + 'Name' + bold_end)
-
-
 # Create a loop to read through each line
 # Extract the first two words from each line as names
-# Extract last three 'words' as d.o.b.
 for line in cl_lines:
     for names in line:
         names = line.split(" ", 2)[0:2]
@@ -20,13 +18,15 @@ for line in cl_lines:
 print("\n\n")
 
 print(bold_start + 'Birthdate' + bold_end)
-
+# Extract last three 'words' as d.o.b.
 for line in cl_lines:
     for dob in line:
         dob = line.split(" ", 2)[2:5]
     print(' '.join(dob))
 
-#    tuple_data = tuple(line)
-#    print(tuple_data)
-   
-
+""" I found this task surprisingly hard! I found myself trying various
+techniques, including trying to learn pandas in order to place the data in to
+some kind of usable form, trying to pass the strings as tuples, and trying
+to use OOP. In the end, I opted for splitting the data in to strings, then
+splitting these and playing around with the presentation of the information.
+I tried to keep the code as short as I could!"""
